07e46b3331061725d72146ed26ee1793208887d1
Facundo Carrion <facundojcarrion@gmail.com>
2023-02-23 15:43:36 -0300

    🪄 update some eslint and prettier rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,13 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react'],
+  plugins: ['astro', 'react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    'multiline-ternary': 'off',
+    'space-before-function-paren': 'off'
   },
   settings: {
     react: {
